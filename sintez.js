6e4ba7ac18c8c034632ac3f40d3041f3fe51194f
darthspectrum <muradianator@gmail.com>
2025-05-10 15:43:55 +0400

    :construction: interpreting sentences

--- sintez.js
+++ sintez.js
@@ -1,4 +1,4 @@
-export { encodeWAV, generatePCM, tokenize };
+export { encodeWAV, generatePCM, tokenize, typeify };
 
 // sample[n]= A ⋅ sin(2 * π * f * (n / R)​)
 
@@ -60,17 +60,32 @@ async function encodeWAV(
 
 const atom = (name) => Symbol.for(name);
 
+const typeify = (token) => {
+  const number = Number.parseFloat(token, 10);
+  return Number.isNaN(number) ? atom(token) : number;
+};
+
 const tokenize = (input) => {
-  const token = []
-  const list = input.split(" ")
-  if(input != ""){
+  const token = [];
+  const list = input.split(" ");
+  if (input != "") {
     for (let i = 0; i < list.length; i++) {
       if (!isNaN(Number(list[i]))) {
-        token[i] = Number(list[i])
+        token[i] = Number(list[i]);
       } else {
-        token[i] = atom(list[i])
+        token[i] = atom(list[i]);
       }
     }
   }
   return token;
+};
+
+const run = (
+  program,
+  definitions = [],
+) => {
+  const tokens = tokenize(program);
+  const value = evaluate(tokens, definitions);
+
+  return value;
 };
