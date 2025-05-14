826e8ed647c9400c7f0189b8cbe2f2b89e77168d
AniGh <anighasabyan08@gmail.com>
2025-05-14 21:33:43 +0400

    tokenized

--- sintez.js
+++ sintez.js
@@ -3,7 +3,7 @@ export { encodeWAV, generatePCM, tokenize };
 // sample[n]= A ⋅ sin(2 * π * f * (n / R)​)
 
 // Where:
-//   A: Amplitude (max value based on bit depth, e.g., 32767 for 16-bit)
+//   A: Amplitude (max list[i] based on bit depth, e.g., 32767 for 16-bit)
 //   f: Frequency (Hz), e.g., middle C = 261.63 Hz
 //   R: Sample rate (samples per second), typically 44100 Hz
 //   n: Sample number (integer), from 0 to R × duration − 1
@@ -61,5 +61,16 @@ async function encodeWAV(
 const atom = (name) => Symbol.for(name);
 
 const tokenize = (input) => {
-  throw new Error("Not implemented");
+  const token = []
+  const list = input.split(" ")
+  if(input != ""){
+    for (let i = 0; i < list.length; i++) {
+      if (!isNaN(Number(list[i]))) {
+        token[i] = Number(list[i])
+      } else {
+        token[i] = atom(list[i])
+      }
+    }
+  }
+  return token;
 };
